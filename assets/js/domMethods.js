a0d5ce72f068ce63b65d807f7b866bde22e9611b
Brendan Kersey <bmkersey@gmail.com>
2022-08-11 02:36:20 -0400

    set up and activated service worker, added everything for PWA-ability

--- assets/js/domMethods.js
+++ assets/js/domMethods.js
@@ -1,40 +1,37 @@
+const createEl = function (htmlString, attrs, ...children) {
+	if (typeof htmlString !== "string") {
+		throw Error("Argument 'htmlString' is required and must be a string");
+	}
 
+	const el = document.createElement(htmlString);
 
-function createEl(htmlString, attrs, ...children) {
-  if (typeof htmlString !== "string") {
-    throw Error("Argument 'htmlString' is required and must be a string");
-  }
+	if (typeof attrs === "object") {
+		for (let key in attrs) {
+			if (key.substring(0, 2) === "on") {
+				el.addEventListener(key.substring(2).toLowerCase(), attrs[key]);
+			} else if (key === "style") {
+				for (let rule in attrs[key]) {
+					el.style[rule] = attrs[key][rule];
+				}
+			} else {
+				el.setAttribute(key, attrs[key]);
+			}
+		}
+	}
 
-  const el = document.createElement(htmlString);
+	children.forEach(function(child) {
+		let node;
 
-  if (typeof attrs === "object") {
-    for (let key in attrs) {
-      if (key.substring(0, 2) === "on") {
-        el.addEventListener(key.substring(2).toLowerCase(), attrs[key]);
-      } else {
-        el.setAttribute(key, attrs[key]);
-      }
-    }
-  }
+		if (child.constructor.name.includes("Element")) {
+			node = child;
+		} else {
+			node = document.createTextNode(child);
+		}
 
-  children.forEach(function(child) {
-    let node;
+		el.appendChild(node);
+	});
 
-    if (child.constructor.name.includes("Element")) {
-      node = child;
-    } else {
-      node = document.createTextNode(child);
-    }
-
-    el.appendChild(node);
-  });
-
-  return el;
-};
-
-
-
-
-
+	return el;
+}
 
 module.exports = createEl;
